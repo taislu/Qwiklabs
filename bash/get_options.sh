3bd9b8e5e0fda0ce20dbf14b8e0d4fd7a37bbe94
Tai Lu <taislu@hotmail.com>
2021-09-30 10:28:02 -0700

    add bash/get_options.sh

---
+++ bash/get_options.sh
@@ -0,0 +1,14 @@
+# Usage : ./get_options.sh -f 'John Smith' -a 25 -u john
+#!/bin/bash
+
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        a) age=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+echo "Username: $username";
+echo "Age: $age";
+echo "Full Name: $fullname";
