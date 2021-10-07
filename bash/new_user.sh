7a5474c13370ebcc4fbdb34a99d81493eda912db
Tai Lu <taislu@hotmail.com>
2021-10-07 10:55:08 -0700

    add bash/new_user.sh

---
+++ bash/new_user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo -e "Username:"
+read username
+if id -u $username > -1; then
+    echo "That username already exists"
+else
+    echo -e "Full Name:"
+    read fullname
+    echo -e "Password:"
+    read password
+    useradd -c "$fullname" $username -p $password
+    if id -u $username > -1; then
+        echo "User created successfully"
+    else
+        echo "An error occurred while creating the user"
+    fi
+    test -d /home/$username && echo "Home Directory Exists" || echo "Home Directory Does Not Exist"
+fi
